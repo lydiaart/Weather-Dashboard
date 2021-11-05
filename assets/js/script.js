694867893993dd40276b60315b1d3540c5ad2877
Lydia <lydia_art@yahoo.com>
2021-11-04 21:34:06 -0500

    fetch current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,86 @@
 var searchCity = document.querySelector("#search-query")
-var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=15b3aa0f567265f18ce1bd88486e5f83"
+var searchBtn = document.querySelector("#search-btn")
+var textFrame = document.querySelector(".text-frame")
+var flexBox = document.querySelector(".flexbox")
+ 
+
+
+
+searchBtn.addEventListener("click", function () {
+    console.log(searchCity.value)
+    getCityWeather(searchCity.value);
+})
 
 function getCityWeather(cityName) {
     var currentWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=15b3aa0f567265f18ce1bd88486e5f83`
     fetch(currentWeatherAPI)
     .then(function (response) {
-        if (response.ok) {
-            response.json().then(function (currentWeatherData) {
-                console.log(currentWeatherData)
-            })
-        }
-
+            return response.json()
     })
+    .then(function (currentWeatherData) {
+        console.log(currentWeatherData) 
+
+          var UVAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentWeatherData.coord.lat}&lon=${currentWeatherData.coord.lon}&units=imperial&appid=15b3aa0f567265f18ce1bd88486e5f83`
+            
+            fetch(UVAPI)
+            .then(function (UVresponse) {
+                    return UVresponse.json()
+             })
+            .then(function (UVData) {
+              
+                console.log(UVData)
+    
+                textFrame.innerHTML = `
+                    <span>
+    
+                       ${currentWeatherData.name}(${moment(currentWeatherData.dt, "X").format("MM/DD/YYYY")})  
+                   
+                       <img class="image"  src="http://openweathermap.org/img/wn/${currentWeatherData.weather[0].icon}.png"     /> 
+                      
+                  </span>
+    
+                   <p>Temp:${currentWeatherData.main.temp}</p>
+                   <p>Wind:${currentWeatherData.wind.speed}</p>
+                   <p>humidity:${currentWeatherData.main.humidity}</p>
+                   <p>UV Index:${UVData.current.uvi}</p>
+                   `
+
+
+                   for (let i = 1; i < UVData.daily.length - 2; i++) {
+                       console.log(UVData.daily[i])
+                         console.log( moment( UVData.daily[i].dt,"X" ).format("MM/DD/YYYY"))
+
+                         flexBox.innerHTML =   flexBox.innerHTML + `
+                         <div class="col-card ">
+                         <div class="card-panel grey lighten-2 cardStyle">
+                           <div class="container">
+                           <span>
+    
+                           ${moment(UVData.daily[i].dt, "X").format("MM/DD/YYYY")}  
+                       
+                           <img class="image"  src="http://openweathermap.org/img/wn/${UVData.daily[i].weather[0].icon}.png"     /> 
+                          
+                      </span>
+        
+                       <p>Temp:${UVData.daily[i].temp.max}</p>
+                       <p>Wind:${UVData.daily[i].wind_speed}</p>
+                       <p>humidity:${UVData.daily[i].humidity}</p>
+                           </div>
+                         </div>
+                       </div>
+                         `
+                       
+                   }
+
+    
+            })
+            
+    })
+
+
+ 
 }
+
+
 
 getCityWeather("Chicago");
